f5d275b559916cb636a720ca71c6fa9ba52e57e0
alim459 <alim459@gmail.com>
2015-06-10 18:26:50 -0700

    alvin added the modesl, but still needs to migrate them

--- TransientFood/app/models.py
+++ TransientFood/app/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
-# CLASSES REQUIRED:
-# Vendor
-# FoodTruck
-# FarmersMarket
-# LatLon???
-# Time???
-# User??
-# Made changes to alvin's branch
+class Vendor(models.Model):
+    name = models.CharField(max_length=60)
+    genre = models.CharField(max_length=60)
+    # location = models.Location # ask if this is right??
+    opening = models.IntegerField
+    closing = models.IntegerField
+    # bogusField = models.IntegerField
+
+
+
+    def __unicode__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
+
+
+    # !!!think about photos/pictures
+
