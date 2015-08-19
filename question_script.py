5937e56c145c1eee91c36f85750e88f6ace8e9ff
alim459 <alim459@gmail.com>
2015-08-19 10:53:08 -0700

    updated paramaterr names, initial guess of 50000

--- question_script.py
+++ question_script.py
@@ -3,7 +3,12 @@ import os
 import urllib2, urllib
 import requests
 
-def make_guess(guess, prev_min, prev_max):
+#params:
+    #guess is the current guess
+    #min is the minimum possible value--true answer is somewhere above min
+    #max is the maximum possible value--true answer is somewhere below the max
+
+def make_guess(guess, min, max):
     url = 'http://52.8.142.239:8080/guess'
     payload = {"guess":guess}
     header = {'Content-type': 'application/json'}
@@ -12,19 +17,27 @@ def make_guess(guess, prev_min, prev_max):
     j = json.loads(response.text)
     answer = j[u'message']
 
+    #terminate the recursive call when the correct answer is obtained
     if answer == 'correct!':
         print("Answer found!!! The correct answer is: " + str(guess))
         return
 
-    #if the current guess is too high, the true answer resides somewhere between the current gues and the previous min
-    if answer == 'too high':
-        print("made a guess of " + str(guess) + ", but it was too high... next guess is: " + str((prev_min+guess)/2))
-        make_guess((guess+prev_min)/2, prev_min, guess)
 
-    #if answer was too low, next guess should be half-way between current guess and the previous max
+    #if the current guess is too high, the true answer resides somewhere between the current guess and the min.
+    #Thus, next guess should be the half-way point between min and the current guess,and max should be
+    #updated to the current guess during the recursive call
+
+    if answer == 'too high':
+        print("made a guess of " + str(guess) + ", but it was too high... next guess is: " + str((min+guess)/2))
+        make_guess((guess+min)/2, min, guess-1)
+
+    #if the current guess is too low, the true answer resides somewhere between the current guess and the max
+    #Thus, next guess should be the half-way point between min and the current guess, and min should be updated
+    #to the current guess during the recusive call
+
     else:
-        print("made a guess of " + str(guess) + ", but it was too low... next guess is: " + str((guess+prev_max)/2))
-        make_guess((guess+prev_max)/2,guess, prev_max)
+        print("made a guess of " + str(guess) + ", but it was too low... next guess is: " + str((guess+max)/2))
+        make_guess((guess+max)/2,guess+1, max)
 
 if __name__ == '__main__':
-    make_guess(5000, 0, 2147483648)
+    make_guess(50000, 0, 2147483648)
